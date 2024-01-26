9f6cb68eb3b5e9993d3aa72187c2526d12c3fb3a
Rhys Johns <rhysjohnsdev@gmail.com>
2024-01-26 20:54:10 +1100

    Organised it a little better so it's more like a structured program now

--- log_to_file.py
+++ log_to_file.py
@@ -1,31 +1,36 @@
-"""
-This could be a cool little program.
+def output():
+    """
+    This could be a cool little program.
 
-I like writing in the terminal, it's a nice experience sometimes. The whole "blank page approach".
+    I like writing in the terminal, it's a nice experience sometimes. The whole "blank page approach".
 
-What if we could have something which just logs my output, with each line being a bullet point.
+    What if we could have something which just logs my output, with each line being a bullet point.
 
-It would by default output the notes into a markdown file, though you could make it configurable.
+    It would by default output the notes into a markdown file, though you could make it configurable.
 
-I'd say it'd be in a file somewhere, or like you could run a command which would let you configure the 
-options.
+    I'd say it'd be in a file somewhere, or like you could run a command which would let you configure the 
+    options.
 
-This could be super cool, the more I talk about it.
-"""
-filename = "console-output.log"
+    This could be super cool, the more I talk about it.
+    """
+    filename = "console-output.log"
 
-try:
-    print(f"Creating output file {filename}...")
-    open(filename, "x")
-    print("File created!")
-except FileExistsError:
-    print("The file already exists! We're in business")
+    try:
+        print(f"Creating output file {filename}...")
+        open(filename, "x")
+        print("File created!")
+    except FileExistsError:
+        print("The file already exists! We're in business")
 
-with open(filename, "a") as file:
-    while True:
-        value = input()
-        if value == "exit":
-            break
+    with open(filename, "a") as file:
+        while True:
+            value = input()
+            if value == "exit":
+                break
 
-        file.write(f"{value}\n")
-        file.flush()
+            file.write(f"{value}\n")
+            file.flush()
+
+
+if __name__ == "__main__":
+    output()
